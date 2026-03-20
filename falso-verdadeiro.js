a9be8022d30a2212bd3c4622ff6a2c78d85f8e0c
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-03-20 15:24:12 +0000

    .

--- falso-verdadeiro.js
+++ falso-verdadeiro.js
@@ -8,6 +8,23 @@ const contaPaga = false;
 // 0 => false
 // 1 => true 
 
-console.log(0 === false)
-console.log(""== false)
-console
+//console.log(0 === false)
+//console.log(""== false)
+//console.log(1 == true);
+
+
+// undefined;
+
+// null;==> vasio ou nada;
+
+let minhavar;
+let  varNull = null;
+
+console.log(minhavar);
+console.log (varNull);
+
+let numero = 3;
+let texto = "alura";
+
+console.log (typeof numero);
+console.log (typeof texto);
