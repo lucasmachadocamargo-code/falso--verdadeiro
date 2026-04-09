f707e83e094fadb4d7746e1ace2695e5ec486e41
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-04-09 14:54:34 +0000

    conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dados
+// boleanos
+
+// conversao implicita
+const numero = 456;
+const numeroString = Number("456");
+
+// nunber()
+// String()
+console.log(numero + numeroString);
+
+// conversao explicitas
