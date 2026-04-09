9f50a2561418907fed2e88568cf38a01fd1bc28b
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-04-09 15:19:45 +0000

    alterando

--- conversoes.js
+++ conversoes.js
@@ -2,11 +2,20 @@
 // boleanos
 
 // conversao implicita
-const numero = 456;
-const numeroString = Number("456");
+//const numero = 456;
+//const numeroString = Number("456");
 
 // nunber()
 // String()
-console.log(numero + numeroString);
+//console.log(numero + numeroString);
 
 // conversao explicitas
+
+// calculadora inteligente
+
+const preco = "100.50";
+const desconto ="10";
+
+console.log(preco - desconto);
+console.log(preco *2;
+    console.log(preco / 2);
