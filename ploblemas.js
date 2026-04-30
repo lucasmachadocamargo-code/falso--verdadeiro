38b0c87582ee169fac70276fd9f7f1b80662c789
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-04-30 15:21:49 +0000

    cpf

---
+++ ploblemas.js
@@ -0,0 +1,6 @@
+const preço ="50";
+const frete = "10";
+
+
+const total = (preço + frete);
+console.log(totel);// resultado:"5010" (Ele grudou os textos em vez de somar)
---
+++ script.js
@@ -0,0 +1,13 @@
+// EXENPLO DE TIPAGEM DINÂMICA
+let dado = 100;
+console.log("valor", dado);
+console.log("tipo", typeof dado);
+
+// mudar o tipo da mesma variavel ( o que define a tipagem dinâmica)
+dado = "ola, sou um texto";
+console.log("novo valor ", dado);
+console.log("novo tipo", typeof dado);
+
+// exenplo da diferença entre let e const
+const cpf = "123.456.789-00";
+console.log("cpf", cpf);
