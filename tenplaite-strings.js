7cf513b4b38f6ae0328bebfba22d2a787cc2881d
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-05-14 15:18:44 +0000

    adicionando

--- tenplaite-strings.js
+++ tenplaite-strings.js
@@ -1,4 +1,9 @@
-const nome = "ju";
-const aprecentaçao = "meu nome é" + nome;
+const nome = "lucas";
+const idade = 2026-2010;
+const cidadeDeNacimento = "Roncador";
 
-console.log(aprecentaçao);
+//const aprecentaçao = "meu nome é" + nome + ", Minha idade é " + idade + " e nasci na cidade " + cidadeNacimento;
+
+const aprecentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNacimento}`;
+
+console.log(aprecentacao);
